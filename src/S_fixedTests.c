45c46d5d1f3501ff1b117b3e5051115e8326b9a1
Can't Sleep <localamnesiac@gmail.com>
2025-08-01 12:27:48 +0300

    More paranoid testing

--- src/S_fixedTests.c
+++ src/S_fixedTests.c
@@ -7,14 +7,14 @@
 
 static int testsCounter = 0;
 
-// are we really THAT inaccurate????
+// Check 4 decimal places max as trigonometry isn't spot-on in fixed point
 #define Epsilon (1e-4)
-#define AssertEq(retardedNum, coolExpr)                                                                                \
+#define AssertEq(num, expr)                                                                                            \
 	do {                                                                                                           \
 		printf("Test #%d: ", testsCounter);                                                                    \
-		double res = fix_to_double((coolExpr));                                                                \
-		if (fabs(res - (retardedNum)) > Epsilon) {                                                             \
-			printf("FAIL\n\n`%s` is %lf != %f\n", #coolExpr, res, (retardedNum));                          \
+		double res = fix_to_double((expr));                                                                    \
+		if (fabs(res - (num)) > Epsilon) {                                                                     \
+			printf("FAIL\n\n\"%s\" is %lf != %f\n", #expr, res, (num));                                    \
 			fflush(stdout);                                                                                \
 			return EXIT_FAILURE;                                                                           \
 		}                                                                                                      \
@@ -22,8 +22,8 @@ static int testsCounter = 0;
 		testsCounter++;                                                                                        \
 	} while (0)
 
-#define DeliciousPi (3.14159265358979323846)
-#define DegToRad (DeliciousPi / 180.0)
+#define Pi (3.1415926535897932384626433832795)
+#define DegToRad (Pi / 180.0)
 
 int main(int argc, char* argv[]) {
 	AssertEq(0.0, FIX_ZERO);
@@ -34,21 +34,21 @@ int main(int argc, char* argv[]) {
 	AssertEq(180.0 * DegToRad, FIX_PI);
 	AssertEq(360 * DegToRad, FIX_DOUBLE_PI);
 
-	AssertEq(13.0, fix_add(fix_from_int(4), fix_from_int(9)));
-	AssertEq(-3.0, fix_sub(fix_from_int(5), fix_from_int(8)));
-	AssertEq(42.0, fix_mul(fix_from_int(6), fix_from_int(7)));
-	AssertEq(7.0 / 6.0, fix_div(fix_from_int(7), fix_from_int(6)));
+	AssertEq(13.0, fix_add(fix_from_int(4L), fix_from_int(9L)));
+	AssertEq(-3.0, fix_sub(fix_from_int(5L), fix_from_int(8L)));
+	AssertEq(42.0, fix_mul(fix_from_int(6L), fix_from_int(7L)));
+	AssertEq(7.0 / 6.0, fix_div(fix_from_int(7L), fix_from_int(6L)));
 
-	AssertEq(4.5, fix_half(fix_from_int(9)));
-	AssertEq(18.0, fix_double(fix_from_int(9)));
+	AssertEq(4.5, fix_half(fix_from_int(9L)));
+	AssertEq(18.0, fix_double(fix_from_int(9L)));
 
 	AssertEq(0.0509, fix_frac(fix_from_double(127.0509)));
 	AssertEq(127.0, fix_floor(fix_from_double(127.0509)));
 	AssertEq(128.0, fix_ceil(fix_from_double(127.0509)));
 
 	AssertEq(123.123, fix_abs(fix_from_double(-123.123)));
-	AssertEq(81.0, fix_sqr(fix_from_int(9)));
-	AssertEq(9.0, fix_sqrt(fix_from_int(81)));
+	AssertEq(81.0, fix_sqr(fix_from_int(9L)));
+	AssertEq(9.0, fix_sqrt(fix_from_int(81L)));
 
 	AssertEq(1.0, fix_tan(FIX_QUARTER_PI));
 	AssertEq(45.0 * DegToRad, fix_asin(fix_sin(FIX_QUARTER_PI)));
