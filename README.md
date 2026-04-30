63f6f908c438ed9083aa7256d6b952bf7c40cc16
Can't Sleep <localamnesiac@gmail.com>
2026-05-01 02:01:42 +0300

    Annual cleanup

--- README.md
+++ README.md
@@ -32,52 +32,54 @@ This is pretty much how every "header-only" library works.
 
 ## Notes
 
-The type you should primarily use with this library is `fix16_t`. `fix32_t` is only used internally for multiplication and division.
+The type you should primarily use with this library is `Fixed`. `Fixed64` is only used internally for multiplication and division.
 
 The trigonometry functions were based on libfixmath's implementations and simplified for this library.
 
 ## Functions
 
-| Function           | Description                                 |
-| ------------------ | ------------------------------------------- |
-| `FxFrom(x)`        | Generic number-to-fixed-point conversion    |
-| `Int2Fx(x)`        | Integer to fixed point                      |
-| `Float2Fx(x)`      | Float to fixed point[^3]                    |
-| `Double2Fx(x)`     | Double to fixed point[^3]                   |
-| `Fx2Int(x)`        | Fixed point to integer                      |
-| `Fx2Float(x)`      | Fixed point to float                        |
-| `Fx2Double(x)`     | Fixed point to double                       |
-| `Fadd(a, b)`       | Add `+`[^1]                                 |
-| `Fsub(a, b)`       | Subtract `-`[^1]                            |
-| `Fmul(a, b)`       | Multiply `*`[^1]                            |
-| `Fdiv(a, b)`       | Divide `/`[^1][^2]                          |
-| `Fmod(a, b)`       | Remainder (modulo) `%`[^2]                  |
-| `Fhalf(x)`         | Halve `x / 2`[^1]                           |
-| `Fdouble(x)`       | Double `x * 2`[^1]                          |
-| `Ffrac(x)`         | Fractional part                             |
-| `Ffloor(x)`        | Round down                                  |
-| `Fceil(x)`         | Round up[^1]                                |
-| `Fabs(x)`          | Absolute value                              |
-| `Fmin(a, b)`       | Minimum of `a` and `b`                      |
-| `Fmax(a, b)`       | Maximum of `a` and `b`                      |
-| `Fclamp(x, a, b)`  | Clamp `x` between `a` and `b`               |
-| `Flerp(a, b, x)`   | Linear interpolation[^1]                    |
-| `Fdeg(x)`          | Radians to degrees[^1]                      |
-| `Frad(x)`          | Degrees to radians[^1]                      |
-| `Fsqr(x)`          | Square[^1]                                  |
-| `Fsqrt(x)`         | Square root[^2]                             |
-| `Fcube(x)`         | Cube[^1]                                    |
-| `Fsin(x)`          | Sine                                        |
-| `Fcos(x)`          | Cosine                                      |
-| `Ftan(x)`          | Tangent[^1][^2]                             |
-| `Fasin(x)`         | Arcsine[^2]                                 |
-| `Facos(x)`         | Arccosine[^2]                               |
-| `Fatan(x)`         | Arctangent[^1][^2]                          |
-| `Fatan2(y, x)`     | Arctangent of `y / x` with benefits[^1][^2] |
+| Function          | Description                                 |
+| ----------------- | ------------------------------------------- |
+| `FxFrom(x)`       | Generic number-to-fixed-point conversion    |
+| `Int2Fx(x)`       | Integer to fixed point                      |
+| `Float2Fx(x)`     | Float to fixed point[^3]                    |
+| `Double2Fx(x)`    | Double to fixed point[^3]                   |
+| `Fx2Int(x)`       | Fixed point to integer                      |
+| `Fx2Float(x)`     | Fixed point to float                        |
+| `Fx2Double(x)`    | Fixed point to double                       |
+| `Fadd(a, b)`      | Add `+`[^1]                                 |
+| `Fsub(a, b)`      | Subtract `-`[^1]                            |
+| `Fmul(a, b)`      | Multiply `*`[^1]                            |
+| `Fdiv(a, b)`      | Divide `/`[^1][^2]                          |
+| `Fmod(a, b)`      | Remainder (modulo) `%`[^2]                  |
+| `Fhalf(x)`        | Halve `x / 2`[^1]                           |
+| `Fdouble(x)`      | Double `x * 2`[^1]                          |
+| `Ffrac(x)`        | Fractional part                             |
+| `Ffloor(x)`       | Round down                                  |
+| `Fceil(x)`        | Round up[^1]                                |
+| `Fabs(x)`         | Absolute value                              |
+| `Fmin(a, b)`      | Minimum of `a` and `b`                      |
+| `Fmax(a, b)`      | Maximum of `a` and `b`                      |
+| `Fclamp(x, a, b)` | Clamp `x` between `a` and `b`               |
+| `Flerp(a, b, x)`  | Linear interpolation[^1]                    |
+| `Fdeg(x)`         | Radians to degrees[^1]                      |
+| `Frad(x)`         | Degrees to radians[^1]                      |
+| `Fsqr(x)`         | Square[^1]                                  |
+| `Fsqrt(x)`        | Square root[^2]                             |
+| `Fcube(x)`        | Cube[^1]                                    |
+| `Fsin(x)`         | Sine                                        |
+| `Fcos(x)`         | Cosine                                      |
+| `Ftan(x)`         | Tangent[^1][^2]                             |
+| `Fasin(x)`        | Arcsine[^2]                                 |
+| `Facos(x)`        | Arccosine[^2]                               |
+| `Fatan(x)`        | Arctangent[^1][^2]                          |
+| `Fatan2(y, x)`    | Arctangent of `y / x` with benefits[^1][^2] |
 
 [^1]: Overflow is possible.
+
 [^2]: Quietly returns `0` on failure.
-[^3]: You should probably use this with common sense. Floats and fixed point don't go well together.<br>For paranoid people, it's more reliable to generate constant fixed point numbers from floats by typing `x * 65536` in a calculator and storing a truncated version of the result with a `fix16_t` cast.
+
+[^3]: You should probably use this with common sense. Floats and fixed point don't go well together.<br>For paranoid people, it's more reliable to generate constant fixed point numbers from floats by typing `x * 65536` in a calculator and storing a truncated version of the result with a `Fixed` cast.
 
 ## Constants
 
--- include/S_fixed.h
+++ include/S_fixed.h
@@ -5,22 +5,22 @@
 #endif
 
 #define FxFBits (16L)
-#define FxWMask ((fix16_t)(0xFFFF0000L))
-#define FxFMask ((fix16_t)(0x0000FFFFL))
+#define FxWMask ((Fixed)(0xFFFF0000L))
+#define FxFMask ((Fixed)(0x0000FFFFL))
 
-typedef int32_t fix16_t;
-typedef int64_t fix32_t;
+typedef int32_t Fixed;
+typedef int64_t Fixed64;
 
-#define FxOne ((fix16_t)(65536L))
-#define FxHalf ((fix16_t)(32768L))
-#define FxZero ((fix16_t)(0L))
+#define FxOne ((Fixed)(65536L))
+#define FxHalf ((Fixed)(32768L))
+#define FxZero ((Fixed)(0L))
 
 #define Fx1 FxOne
 #define Fx0 FxZero
 
-#define Int2Fx(x) ((fix16_t)((int32_t)(x) * (int32_t)Fx1))
-#define Float2Fx(x) ((fix16_t)((float)(x) * (float)Fx1))    // ! Unsafe !
-#define Double2Fx(x) ((fix16_t)((double)(x) * (double)Fx1)) // ! Unsafe !
+#define Int2Fx(x) ((Fixed)((int32_t)(x) * (int32_t)Fx1))
+#define Float2Fx(x) ((Fixed)((float)(x) * (float)Fx1))    // ! Unsafe !
+#define Double2Fx(x) ((Fixed)((double)(x) * (double)Fx1)) // ! Unsafe !
 
 #define Fx2Int(x) ((int32_t)(x) / (int32_t)Fx1)
 #define Fx2Float(x) ((float)(x) / (float)(Fx1))    // ! Unsafe !
@@ -37,221 +37,210 @@ typedef int64_t fix32_t;
 // Use C11 `_Generic` tricks, if available:
 #if __STDC_VERSION__ >= 201112L
 
-#define FxFrom(x)                                                              \
-	_Generic((x),                                                          \
-		int: Int2Fx(x),                                                \
-		long int: Int2Fx(x),                                           \
-		float: Float2Fx(x),                                            \
-		double: Double2Fx(x))
+#define FxFrom(x) _Generic((x), int: Int2Fx(x), long int: Int2Fx(x), float: Float2Fx(x), double: Double2Fx(x))
 
 #endif
 
-#define Fadd(a, b) (((fix16_t)(a)) + ((fix16_t)(b)))
+#define Fadd(a, b) (((Fixed)(a)) + ((Fixed)(b)))
 #define FxAdd Fadd
 
-#define Fsub(a, b) (((fix16_t)(a)) - ((fix16_t)(b)))
+#define Fsub(a, b) (((Fixed)(a)) - ((Fixed)(b)))
 #define FxSub Fsub
 
-#define Fmul(a, b) ((fix16_t)((((fix32_t)(a)) * ((fix32_t)(b))) >> FxFBits))
+#define Fmul(a, b) ((Fixed)((((Fixed64)(a)) * ((Fixed64)(b))) >> FxFBits))
 #define FxMul Fmul
 
-fix16_t Fdiv(const fix16_t a, const fix16_t b)
+Fixed Fdiv(register Fixed a, register Fixed b)
 #define FxDiv Fdiv
 #ifdef FIX_IMPLEMENTATION
 {
-	if (b == Fx0)
-		return Fx0;
-	return (fix16_t)(((fix32_t)(a) << FxFBits) / (fix32_t)(b));
+    return (b == Fx0) ? Fx0 : (Fixed)(((Fixed64)(a) << FxFBits) / (Fixed64)(b));
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fmod(const fix16_t a, const fix16_t b)
+Fixed Fmod(register Fixed a, register Fixed b)
 #define FxMod Fmod
 #ifdef FIX_IMPLEMENTATION
 {
-	if (b == Fx0)
-		return Fx0;
-	return a % b;
+    return (b == Fx0) ? Fx0 : (Fixed)(a % b);
 }
 #else
-	;
+    ;
 #endif
 
-#define Fhalf(a) (((fix16_t)(a)) >> 1L)
+#define Fhalf(a) (((Fixed)(a)) >> 1L)
 #define FxHalve Fhalf
 
-#define Fdouble(a) (((fix16_t)(a)) << 1L)
+#define Fdouble(a) (((Fixed)(a)) << 1L)
 #define FxDouble Fdouble
 
-#define Ffrac(x) (((fix16_t)(x)) & FxFMask)
+#define Ffrac(x) (((Fixed)(x)) & FxFMask)
 #define FxFrac Ffrac
 
-#define Ffloor(x) (((fix16_t)(x)) & FxWMask)
+#define Ffloor(x) (((Fixed)(x)) & FxWMask)
 #define FxFloor Ffloor
 
-fix16_t Fceil(const fix16_t x)
+Fixed Fceil(register Fixed x)
 #define FxCeil Fceil
 #ifdef FIX_IMPLEMENTATION
 {
-	return Ffrac(x) ? Fadd(Ffloor(x), Fx1) : x;
+    return Ffrac(x) ? Fadd(Ffloor(x), Fx1) : x;
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fabs(const fix16_t x)
+Fixed Fabs(register Fixed x)
 #define FxAbs Fabs
 #ifdef FIX_IMPLEMENTATION
 {
-	return x < Fx0 ? -x : x;
+    return (x < Fx0) ? -x : x;
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fmin(const fix16_t a, const fix16_t b)
+Fixed Fmin(register Fixed a, register Fixed b)
 #define FxMin Fmin
 #ifdef FIX_IMPLEMENTATION
 {
-	return a < b ? a : b;
+    return (a < b) ? a : b;
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fmax(const fix16_t a, const fix16_t b)
+Fixed Fmax(register Fixed a, register Fixed b)
 #define FxMax Fmax
 #ifdef FIX_IMPLEMENTATION
 {
-	return a > b ? a : b;
+    return (a > b) ? a : b;
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fclamp(const fix16_t x, const fix16_t a, const fix16_t b)
+#define Fclamp(x) Fmin(Fmax(x, a), b)
 #define FxClamp Fclamp
-#ifdef FIX_IMPLEMENTATION
-{
-	return Fmin(Fmax(x, a), b);
-}
-#else
-	;
-#endif
 
-fix16_t Flerp(const fix16_t a, const fix16_t b, const fix16_t x)
+Fixed Flerp(register Fixed a, register Fixed b, register Fixed x)
 #define FxLerp Flerp
 #ifdef FIX_IMPLEMENTATION
 {
-	return Fadd(a, Fmul(Fsub(b, a), x));
+    return Fadd(a, Fmul(Fsub(b, a), x));
 }
 #else
-	;
+    ;
 #endif
 
-#define FxPi ((fix16_t)(205887L))
-#define FxPi4 (FxPi >> 2L)
-#define FxPi2 (FxPi >> 1L)
-#define Fx2Pi (FxPi << 1L)
-#define Fx3Pi4 ((Fx2Pi + FxPi) >> 2L)
+#define FxPi ((Fixed)(205887L))
+#define FxPi4 ((Fixed)(FxPi >> 2L))
+#define FxPi2 Fhalf(FxPi)
+#define Fx2Pi Fdouble(FxPi)
+#define Fx3Pi4 ((Fixed)(Fadd(Fx2Pi, FxPi) >> 2L))
 
 #define FxDeg(x) Fmul(x, 3754936L)
 #define FxRad(x) Fmul(x, 1144L)
 
-fix16_t Fsqr(const fix16_t x)
+Fixed Fsqr(register Fixed x)
 #define FxSqr Fsqr
 #ifdef FIX_IMPLEMENTATION
 {
-	return Fmul(x, x);
+    return Fmul(x, x);
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fcube(const fix16_t x)
+Fixed Fcube(register Fixed x)
 #define FxCube Fcube
 #ifdef FIX_IMPLEMENTATION
 {
-	return Fmul(x, Fsqr(x));
+    return Fmul(x, Fsqr(x));
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fsqrt(const fix16_t x)
+Fixed Fsqrt(register Fixed x)
 #define FxSqrt Fsqrt
 #ifdef FIX_IMPLEMENTATION
 {
-	if (x <= Fx0)
-		return Fx0;
+    if (x <= Fx0)
+        return Fx0;
 
-	// https://github.com/PetteriAimonen/libfixmath/blob/master/libfixmath/fix16_sqrt.c
-	uint32_t num = (uint32_t)x;
-	uint32_t bit = (num & 0xFFF00000L) ? (1L << 30L) : (1L << 18L);
-	while (bit > num)
-		bit >>= 2L;
+    // https://github.com/PetteriAimonen/libfixmath/blob/master/libfixmath/fix16_sqrt.c
+    uint32_t num = (uint32_t)x;
+    uint32_t bit = (num & 0xFFF00000L) ? (1L << 30L) : (1L << 18L);
+    while (bit > num)
+        bit >>= 2L;
 
-	uint32_t out = 0L;
-	for (uint8_t n = 0L; n < 2L; n++) {
-		while (bit > 0L) {
-			if (num >= (out + bit)) {
-				num -= out + bit;
-				out = (out >> 1L) + bit;
-			} else {
-				out >>= 1L;
-			}
-			bit >>= 2L;
-		}
-		if (n == 0L) {
-			if (num > FxFMask) {
-				num -= out;
-				num = (num << FxFBits) - FxHalf;
-				out = (out << FxFBits) + FxHalf;
-			} else {
-				num <<= FxFBits;
-				out <<= FxFBits;
-			}
-			bit = 1L << (FxFBits - 2L);
-		}
-	}
+    uint32_t out = 0L;
+    for (uint8_t n = 0L; n < 2L; n++) {
+        while (bit > 0L) {
+            if (num >= (out + bit)) {
+                num -= out + bit;
+                out = (out >> 1L) + bit;
+            } else {
+                out >>= 1L;
+            }
 
-	if (num > out)
-		out++;
-	return (fix16_t)out;
+            bit >>= 2L;
+        }
+
+        if (n == 0L) {
+            if (num > FxFMask) {
+                num -= out;
+                num = (num << FxFBits) - FxHalf;
+                out = (out << FxFBits) + FxHalf;
+            } else {
+                num <<= FxFBits;
+                out <<= FxFBits;
+            }
+
+            bit = 1L << (FxFBits - 2L);
+        }
+    }
+
+    if (num > out)
+        out++;
+
+    return (Fixed)out;
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fsin(fix16_t x)
+Fixed Fsin(register Fixed x)
 #define FxSin Fsin
 #ifdef FIX_IMPLEMENTATION
 {
-	x = x % Fx2Pi;
-	while (x > FxPi)
-		x -= Fx2Pi;
-	while (x < -FxPi)
-		x += Fx2Pi;
+    x = x % Fx2Pi;
+    while (x > FxPi)
+        x -= Fx2Pi;
+    while (x < -FxPi)
+        x += Fx2Pi;
 
-	// https://github.com/PetteriAimonen/libfixmath/blob/master/libfixmath/fix16_trig.c#L89
-	const fix16_t x2 = Fsqr(x);
-	fix16_t out = x;
-	x = Fmul(x, x2);
-	out -= x / 6L;
-	x = Fmul(x, x2);
-	out += x / 120L;
-	x = Fmul(x, x2);
-	out -= x / 5040L;
-	x = Fmul(x, x2);
-	out += x / 362880L;
-	x = Fmul(x, x2);
-	out -= x / 39916800L;
-	return out;
+    // https://github.com/PetteriAimonen/libfixmath/blob/master/libfixmath/fix16_trig.c#L89
+    const Fixed x2 = Fsqr(x);
+    Fixed out = x;
+    x = Fmul(x, x2);
+    out -= x / 6L;
+    x = Fmul(x, x2);
+    out += x / 120L;
+    x = Fmul(x, x2);
+    out -= x / 5040L;
+    x = Fmul(x, x2);
+    out += x / 362880L;
+    x = Fmul(x, x2);
+    out -= x / 39916800L;
+
+    return out;
 }
 #else
-	;
+    ;
 #endif
 
 #define Fcos(x) (Fsin(Fadd(x, FxPi2)))
@@ -263,49 +252,51 @@ fix16_t Fsin(fix16_t x)
 #define Fatan(x) (Fatan2(x, Fx1))
 #define FxAtan Fatan
 
-fix16_t Ftan(fix16_t x)
+Fixed Ftan(register Fixed x)
 #define FxTan Ftan
 #ifdef FIX_IMPLEMENTATION
 {
-	return Fdiv(Fsin(x), Fcos(x));
+    return Fdiv(Fsin(x), Fcos(x));
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fatan2(fix16_t y, fix16_t x)
+Fixed Fatan2(register Fixed y, register Fixed x)
 #define FxAtan2 Fatan2
 #ifdef FIX_IMPLEMENTATION
 {
-	// https://github.com/PetteriAimonen/libfixmath/blob/master/libfixmath/fix16_trig.c#L150
-	const fix16_t mask = y >> (sizeof(fix16_t) * 7L);
-	const fix16_t abs_y = (y + mask) ^ mask;
+    // https://github.com/PetteriAimonen/libfixmath/blob/master/libfixmath/fix16_trig.c#L150
+    const Fixed mask = y >> (sizeof(Fixed) * 7L);
+    const Fixed abs_y = (y + mask) ^ mask;
 
-	fix16_t angle = Fx0;
-	if (x >= 0L) {
-		const fix16_t r = Fdiv(x - abs_y, x + abs_y);
-		angle = Fmul(12864L, Fcube(r)) - Fmul(64336L, r) + FxPi4;
-	} else {
-		const fix16_t r = Fdiv(x + abs_y, abs_y - x);
-		angle = Fmul(12864L, Fcube(r)) - Fmul(64336L, r) + Fx3Pi4;
-	}
+    Fixed angle = Fx0;
+    if (x >= 0L) {
+        const Fixed r = Fdiv(x - abs_y, x + abs_y);
+        angle = Fmul(12864L, Fcube(r)) - Fmul(64336L, r) + FxPi4;
+    } else {
+        const Fixed r = Fdiv(x + abs_y, abs_y - x);
+        angle = Fmul(12864L, Fcube(r)) - Fmul(64336L, r) + Fx3Pi4;
+    }
 
-	return y < Fx0 ? -angle : angle;
+    return (y < Fx0) ? -angle : angle;
 }
 #else
-	;
+    ;
 #endif
 
-fix16_t Fasin(const fix16_t x)
+Fixed Fasin(register Fixed x)
 #define FxAsin Fasin
 #ifdef FIX_IMPLEMENTATION
 {
-	if (x > Fx1 || x < -Fx1)
-		return Fx0;
-	fix16_t out = Fsub(Fx1, Fsqr(x));
-	out = Fdiv(x, Fsqrt(out));
-	return Fatan(out);
+    if (x > Fx1 || x < -Fx1)
+        return Fx0;
+
+    Fixed out = Fsub(Fx1, Fsqr(x));
+    out = Fdiv(x, Fsqrt(out));
+
+    return Fatan(out);
 }
 #else
-	;
+    ;
 #endif
